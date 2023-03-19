38c00b840addaa834ce959ec0378aacdd010e6ef
matheus-frota <matheusfrota@hotmail.com.br>
2023-03-19 00:47:32 -0300

    refactor: Refatorando alguns códigos dentro da estrutura do while e colocando-os criando funções

--- trigger_pixel_color/trigger_bot.py
+++ trigger_pixel_color/trigger_bot.py
@@ -22,14 +22,6 @@ def get_screeshot(type = 'all'):
         return pag.screenshot(region=(left+WIDTH/2-5, top+HEIGHT/2+15, 30, 70))
 
 
-def check_key_pressed():
-    if not (keyboard.is_pressed('w') or keyboard.is_pressed('a') or keyboard.is_pressed('s') or keyboard.is_pressed('d')):
-        return False
-    elif keyboard.is_pressed('1') or keyboard.is_pressed('2') or keyboard.is_pressed('3') or keyboard.is_pressed('f'):
-        return True
-    return False
-
-
 def is_mouse_down():    # Returns true if the left mouse button is pressed
     lmb_state = win32api.GetKeyState(0x01)
     return lmb_state < 0
@@ -39,23 +31,36 @@ def show_vision_trigger(arr):
     plt.imshow(arr, cmap='gray')
     plt.show()
 
+
+def descriptive_statistics(arr, show: bool = False):
+    r_sum, r_mean, r_median, r_std = np.sum(arr), np.mean(arr), np.median(arr), np.std(arr)
+    if show:
+        print(f"Media: {r_mean} - Soma: {r_sum} - Mediana: {r_median} - Std: {r_std}")
+    return np.sum(arr), np.mean(arr), np.median(arr), np.std(arr)
+
+
+def enemy(r_mean):
+    return (r_mean >= 5) and (r_mean < 20)
+
+
+def fire(screen):
+    red_layer = np.array(screen)[:, :, 0]
+    
+    red_layer[red_layer >= 250] = 255
+    red_layer[red_layer < 250] = 0
+
+    _, r_mean, _, _ = descriptive_statistics(red_layer)
+
+    if enemy(r_mean):
+        pag.mouseDown()
+        time.sleep(0.2)
+        pag.mouseUp()
+        return True
+    return False
+
+
 while True:
     screen = get_screeshot('test')
 
-    arr = np.array(screen)
-
-    r = arr[:, :, 0]
-    
-    r[r >= 250] = 255
-    r[r < 250] = 0
-
-    r_sum = np.sum(r)
-    r_mean = np.mean(r)
-    r_median = np.median(r)
-    r_std = np.std(r)
-    print(f"Media: {r_mean} - Soma: {r_sum} - Mediana: {r_median} - Std: {r_std}")
-    if (r_mean >= 5) and (r_mean < 20):
-        pag.mouseDown()
-        time.sleep(0.4)
-        pag.mouseUp()
+    fire(screen)
     
